39531dc4ecab33ba425946fc2af19724365cd419
PhillipHage202 <73121315+PhillipHage202@users.noreply.github.com>
2020-10-29 11:52:45 +0000

    Add files via upload

---
+++ python-testing/applications/list_of_squares.py
@@ -0,0 +1,7 @@
+def list_of_squares(n):
+    d=dict()
+    for i in range(1,n+1):
+        d[i]=i*i
+    return d
+
+## it prints > {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
---
+++ python-testing/applications/thisisatest.py
@@ -0,0 +1,7 @@
+u=input
+d=dict()
+for i in range(1,10):
+    d[i]=i%i
+    
+     
+print(d)
