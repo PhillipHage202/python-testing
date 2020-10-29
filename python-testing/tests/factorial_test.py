7bc4ee51410e3583e691ba10962441ec5fd9de7e
PhillipHage202 <73121315+PhillipHage202@users.noreply.github.com>
2020-10-29 12:15:16 +0000

    Add files via upload

---
+++ python-testing/tests/factorial_test.py
@@ -0,0 +1,8 @@
+from applications import factorial
+import pytest
+
+def test_factorial_with_0():
+    assert factorial.fact(0) ==1
+
+def test_factorial_with_5():
+    assert factorial.fact(5) ==120
